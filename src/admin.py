418787e54f8e6a4e62aa37f6424bf8c6c5f69fee
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-01-14 13:38:22 -0500

    added table to admin

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Person
+from models import db, User, Person, TextFile
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
