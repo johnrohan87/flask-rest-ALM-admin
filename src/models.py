8c3acd75a28d02c178ce464d79d125b685b0ebe1
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-07-13 23:42:22 -0400

    testing fix

--- src/models.py
+++ src/models.py
@@ -71,7 +71,7 @@ class Person(db.Model):
 
 class TextFile(db.Model):
     __tablename__ = "textfile"
-    id = db.Column('textfile_id', db.Integer, primary_key=True)
+    #id = db.Column('textfile_id', db.Integer, primary_key=True)
     person_id = db.Column('owner_id', db.Integer, db.ForeignKey('person.id'))
     ip = db.Column(db.String(20),unique=False, nullable=False)
     update_feed = db.Column(db.Boolean, nullable=False)
