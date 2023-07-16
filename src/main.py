a04aa0a1e311de4bd2eed64c16cf7c223100504f
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-07-16 14:52:20 -0400

    testing textfile post fix

--- src/main.py
+++ src/main.py
@@ -98,7 +98,8 @@ def textfile():
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
         if 'update_feed' not in body:
-            raise APIException('You need to specify the update_feed'+jsonify(request.json), status_code=400)
+            print(jsonify(request.json))
+            raise APIException('You need to specify the update_feed', status_code=400)
         if 'url' not in body:
             raise APIException('You need to specify the url', status_code=400)
         if 'textfile' not in body:
