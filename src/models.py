9cc527e2dd3f4c877c0f99dc1d8e4c481ff1c32e
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-07-16 09:33:16 -0400

    testing db changes

--- src/models.py
+++ src/models.py
@@ -30,7 +30,7 @@ class Person(db.Model):
     roles = db.Column(db.Integer, unique=False, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     salt = db.Column(db.String(255), nullable=False)
-    text_files = relationship('TextFile', back_populates='person', lazy=True, cascade='all,delete')
+    text_file = relationship('TextFile', back_populates='person', lazy=True, cascade='all,delete')
 
     # tell python how to print the class object on the console
     def __repr__(self):
