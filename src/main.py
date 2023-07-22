979e0c98edee00731977e7d1fbeb6154437c7616
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-07-21 22:32:13 -0400

    teting migration

--- src/main.py
+++ src/main.py
@@ -83,7 +83,7 @@ def login():
 
 
 @RateLimiter(max_calls=10, period=1)
-@app.route("/textfile", methods=["GET", "POST"])
+@app.route("/textfile", methods=["GET","PUT","POST"])
 @jwt_required()
 def textfile():
     if request.method == 'GET':
@@ -92,6 +92,37 @@ def textfile():
         for item in range(len(files)):
             values.append({'list position': item, 'persons id': files[item].id, 'ip': files[item].ip, "update feed": files[item].update_feed, "url": files[item].url, "file text": files[item].text}) 
         return values
+    
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", {data: body}, status_code=400)
+        if 'update_feed' not in body:
+            data = get_resource()
+
+            raise APIException('You need to specify the update_feed', status_code=400)
+        if 'url' not in body:
+            raise APIException('You need to specify the url', status_code=400)
+        if 'textfile' not in body:
+            raise APIException('You need to specify the textfile', status_code=400)
+        #if 'ip' not in body:
+            #raise APIException('You need to specify the ip', status_code=400)
+        #current_identity = get_jwt_identity()
+        #current_email = Person.serialize(current_user)
+        #payload = current_email
+        #payload.update({'current_identity' : current_identity})
+        try:
+            #ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+            print(body)
+            put_payload = TextFile(Person(person=body['person_id']), ip="0.0.0.0", url=body['url'], update_feed=body['update_feed'], text=body['textfile'])
+            db.session.add(put_payload)
+            db.session.commit()
+
+            return jsonify({
+            "request":body}), 200
+        except Exception as error:
+            print(repr(error))
+            return "!!!!" + {'args':error.args,'error':error}
 
     if request.method == 'POST':
         body = request.get_json()
