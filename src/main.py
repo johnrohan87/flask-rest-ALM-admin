fb02036098406237357783121153453498651baa
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-10-28 21:09:13 -0400

    testing

--- src/main.py
+++ src/main.py
@@ -284,7 +284,7 @@ def feedpost():
             #values.append({'list position': item, 'id': post[item].id, 'feed_id': post[item].feed_id, "title": post[item].title, "link": post[item].link, "published": post[item].published}) 
         for item in range(len(post)):
             print(post[item])
-            print(item)
+            print(post)
             for name,dict_ in post[item]:
                 values.append({item:{dict_:name}})
         return jsonify(values),200
