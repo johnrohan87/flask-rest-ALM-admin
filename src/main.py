08030152cb82618ca9c14600e816d3f48f5a2d5b
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-10-29 23:09:11 -0400

    testing add entries to db

--- src/main.py
+++ src/main.py
@@ -355,7 +355,7 @@ def addrss():
                 if item.has_key('title'): tmpTitle = item.title 
                 else: tmpTitle = "No Title"
                 print(tmpTitle)
-                if item.has_key('link'): tmpLink = jsonify(item.link) 
+                if item.has_key('link'): tmpLink = json.dumps(item.link) 
                 else: tmpLink = "No Link"
                 print(tmpLink)
                 if item.has_key('published'): tmpPublished = item.published 
@@ -370,7 +370,7 @@ def addrss():
                 if item.has_key('summary'): tmpSummary = item.summary 
                 else: tmpSummary = "No Summary"
                 print(tmpSummary)
-                if item.has_key('tags'): tmpTags = jsonify(item.tags) 
+                if item.has_key('tags'): tmpTags = json.dumps(item.tags) 
                 else: tmpTags = "No Tags"
                 print(tmpTags)
 
