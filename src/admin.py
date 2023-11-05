17ceef8de6eb4e5dc94bbe99d990341cb06e82e7
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-05 09:40:16 -0500

    testing db relationship diaplays

--- src/admin.py
+++ src/admin.py
@@ -8,12 +8,16 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
+    class PersonMV(ModelView):
+        column_display_pk = True # optional, but I like to see the IDs in the list
+        column_hide_backrefs = False
+
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Person, db.session))
-    admin.add_view(ModelView(TextFile, db.session))
-    admin.add_view(ModelView(FeedPost, db.session))
+    admin.add_view(PersonMV(Person, db.session))
+    admin.add_view(PersonMV(TextFile, db.session))
+    admin.add_view(PersonMV(FeedPost, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
