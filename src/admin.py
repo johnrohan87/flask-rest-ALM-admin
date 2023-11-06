05c1751f2e5257d0e3b88a84ff0edae5ab2be39f
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-05 22:06:28 -0500

    testing db relationship diaplays

--- src/admin.py
+++ src/admin.py
@@ -23,7 +23,7 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Person, db.session))
-    admin.add_view(ManySideObjView(TextFile, db.session))
+    admin.add_view(TextFileMV(TextFile, db.session))
     admin.add_view(ModelView(FeedPost, db.session))
 
     # You can duplicate that line to add mew models
