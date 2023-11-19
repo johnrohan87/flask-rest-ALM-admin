0e5456b77eabe6f3e470e175675bf6396967c00a
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-19 18:10:19 -0500

    testing textfile admin change

--- src/admin.py
+++ src/admin.py
@@ -16,7 +16,7 @@ def setup_admin(app):
         #ignore_hidden = False
         #create_modal = True
         #edit_modal = True
-        form_columns = ['person_id', 'person.id', 'feeds.id', 'ip', 'update_feed', 'url', 'text']
+        form_columns = ['person_id', 'ip', 'update_feed', 'url', 'text']
         #column_list = ('id', 'person_id', 'person', 'feeds', 'ip', 'update_feed', 'url', 'text')
         #inline_modle = [(Person,dict(form_columns=['id','email','roles','text_files'])),]
 
