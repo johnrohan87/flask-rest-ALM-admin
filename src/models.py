38ff5d460f06f3afb25466b1502e055cd97475a3
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-19 18:03:38 -0500

    testing textfile admin change

--- src/models.py
+++ src/models.py
@@ -38,7 +38,7 @@ class Person(db.Model):
     password = db.Column(db.String(255), nullable=False)
     salt = db.Column(db.String(255), nullable=False)
     is_fresh = db.Column(db.Boolean, nullable=False)
-    text_files = relationship('TextFile', back_populates='person', collection_class=set, lazy=True, cascade='all,delete')
+    text_files = relationship('TextFile', back_populates='person', uselist=True, collection_class=set, lazy=True, cascade='all,delete')
 
     # tell python how to print the class object on the console
     def __repr__(self):
