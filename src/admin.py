02d014de49a203939ca64a97d52e5899977d9b24
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-19 21:23:01 -0500

    testing mdl change

--- src/admin.py
+++ src/admin.py
@@ -23,6 +23,7 @@ def setup_admin(app):
         #column_display_pk = True 
         #column_hide_backrefs = False
         #column_display_all_relations = True
+        
         form_columns = ['feed_id', 'title', 'link', 'published', 'published_parsed', 'author', 'summary', 'tags']
         column_list = ('id', 'feed_id', 'feed', 'title', 'link', 'published', 'published_parsed', 'author', 'summary', 'tags')
 
