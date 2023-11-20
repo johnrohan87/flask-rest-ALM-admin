6c8a89c6193edbe72a2e6e0b2bc759cfe00b0c2d
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-19 21:15:18 -0500

    testing textfile admin change

--- src/admin.py
+++ src/admin.py
@@ -20,9 +20,9 @@ def setup_admin(app):
         column_list = ('id', 'person_id', 'person', 'feeds', 'ip', 'update_feed', 'url', 'text')
 
     class FeedMV(ModelView):
-        column_display_pk = True 
-        column_hide_backrefs = False
-        column_display_all_relations = True
+        #column_display_pk = True 
+        #column_hide_backrefs = False
+        #column_display_all_relations = True
         form_column = ['feed_id', 'title', 'link', 'published', 'published_parsed', 'author', 'summary', 'tags']
         column_list = ('id', 'feed_id', 'feed', 'title', 'link', 'published', 'published_parsed', 'author', 'summary', 'tags')
 
