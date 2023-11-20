7fbbeae571ac493a8c00ab4275847fdf514ba5e0
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2023-11-19 21:26:29 -0500

    testing mdl change

--- src/models.py
+++ src/models.py
@@ -91,7 +91,7 @@ class TextFile(db.Model):
     update_feed = db.Column(db.Boolean, nullable=False)
     url = db.Column(db.Text, nullable=False)
     text = db.Column(db.Text, nullable=False)
-    person = relationship(Person, back_populates='text_files')
+    person = relationship('Person', back_populates='text_files')
     feeds = relationship('FeedPost', back_populates='feed', collection_class=set, lazy=True, cascade='all,delete')
 
 
