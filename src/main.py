1087ca89e22ff7c99700ca81a16ad74a6f11e73f
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-02-08 02:12:13 -0500

    testing

--- src/main.py
+++ src/main.py
@@ -438,7 +438,18 @@ def handle_preflight():
     
 # adding todo app
 @RateLimiter(max_calls=10, period=1)
-@app.route("/api/todos/<int:todo_id>", methods=["GET","POST","PUT","DELETE"])
+@app.route("/api/todos", methods=["GET"])
+@jwt_required(fresh=True)
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def todoAppGet():
+    if request.method == 'GET':
+        user_id = get_jwt_identity()
+        todos = Todo.query.filter_by(userID=user_id).all()
+        todos_list = [{'id': todo.id, 'text': todo.text} for todo in todos]
+        return jsonify(todos_list), 200
+
+@RateLimiter(max_calls=10, period=1)
+@app.route("/api/todos/<int:todo_id>", methods=["POST","PUT","DELETE"])
 @jwt_required(fresh=True)
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def todoApp():
