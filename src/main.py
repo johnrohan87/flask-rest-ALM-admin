ada567f6d25f244c31efe7d92404a6dc07578698
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-02-18 22:03:31 -0500

    testing error mods

--- src/main.py
+++ src/main.py
@@ -109,7 +109,7 @@ def refresh():
         fresh = user.is_fresh
         print(fresh)
         print(user.serialize())
-        new_access_token = create_access_token(identity=user.serialize(), fresh=fresh)
+        new_access_token = create_access_token(identity=user.id, fresh=fresh)
         print(new_access_token)
 
         return jsonify({"access_token": new_access_token}), 200
